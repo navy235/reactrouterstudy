66e8f154f98f3a3d23c0670331362a1c67a9f32a
navy235 <shenhaijunmail@163.com>
2015-05-14 11:28:54 -0500

    init

---
+++ webpack.client.config.js
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+var path = require('path');
+var webpack = require("webpack");
+
+//var commonsPlugin =
+//  new webpack.optimize.CommonsChunkPlugin('common.js');
+var config = {
+    context: path.join(__dirname, '/public'),
+    entry: {
+        client: ['./scripts/routes']
+    },
+    output: {
+        path: path.join(__dirname, '/public/build'),
+        publicPath: '/build/', //for the chunks to be downloaded from.
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].chunk.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.dust$/,
+                loader: "dust"
+            }
+        ],
+    },
+    resolve: {
+        // Allow require('./blah') to require blah.jsx
+        extensions: ['', '.js', '.dust']
+    },
+    externals: {
+        // Use external version of React (from CDN for client-side, or bundled with ReactJS.NET for server-side)
+        backbone: 'Backbone',
+        underscore: '_',
+        jquery: 'jQuery',
+        'dust-core': 'dust'
+    },
+    plugins: [
+        // minimize
+        // new webpack.optimize.UglifyJsPlugin({
+        //   minimize: true
+        // })
+    ]
+
+    //plugins:[new webpack.optimize.CommonsChunkPlugin('client','client.bundle.js')]
+};
+
+module.exports = config;
+webpack(config, function (err, stats) {
+    if (err) {
+        console.log('Bundling error.');
+    } else {
+        console.log('Bundling complete.');
+    }
+
+});
